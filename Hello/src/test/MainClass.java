bbdad70a5631b341a8de304b6e8934f3f5a58c86
kimgura <oli999@naver.com>
2022-04-28 17:01:39 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,21 @@
+package test;
+
+public class MainClass {
+	//run(실행) 했을때 실행의 흐름이 시작 되는 main 메소드 
+	public static void main2(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+		String str="kimgura";
+		
+		int num1=10;
+		
+		boolean isRun=true;
+		
+	}
+	
+}
+
+
+
+
